686ac322e8c98f337faa361d0bf372da3781470e
Alxander64 <alexanderursu99@outlook.com>
2018-11-15 20:36:04 -0500

    Basic GameState saving with pickle

--- src/utils/menu_input.py
+++ src/utils/menu_input.py
@@ -1,12 +1,23 @@
 from game.game_state import GameState
+import pickle
 
 def new_game():
     print("NEW GAME")
     g = GameState()
     print(g)
+    save = open("save.p", "wb+")
+    pickle.dump(g, save)
+    save.close()
 
 def load_save():
     print("LOAD SAVE")
+    try:
+        save = open("save.p", "rb")
+        g = pickle.load(save)
+        save.close()
+        print(g)
+    except FileNotFoundError:
+        print("NO SAVE FILE")
 
 def help():
     help = open('texts/help.txt', 'r')
