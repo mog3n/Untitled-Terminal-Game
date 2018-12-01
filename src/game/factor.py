dd992a607a814ea6d42f7fe19b1a9f61548701ec
Alxander64 <alexanderursu99@outlook.com>
2018-11-30 23:25:05 -0500

    remove factors

--- src/game/factor.py
+++
@@ -1,9 +0,0 @@
-class Factor:
-
-    def __init__(self):
-        self._total = 0
-        self._good = 0
-        self._bad = 0
-
-    def __str__(self):
-        return "Exp: {} Pos: {} Neg: {}".format(self._total, self._good, self._bad)
--- src/game/game_state.py
+++ src/game/game_state.py
@@ -1,12 +1,10 @@
-from game.factor import Factor
-
 class GameState:
 
     def __init__(self):
         self._rounds = 0
-        self._state = {"Circles": Factor(),
-                       "Wealth": Factor(),
-                       "Mentality": Factor()}
+        self._state = {"Circles": 0,
+                       "Wealth": 0,
+                       "Mentality": 0}
 
     def __str__(self):
         str = "Rounds: {}\n".format(self._rounds)
