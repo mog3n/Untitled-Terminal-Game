e2c616bd5df44feea6c26cbf7f87f3b191deeef5
Alxander64 <alexanderursu99@outlook.com>
2018-11-30 23:30:39 -0500

    updated game state

--- src/game/game_state.py
+++ src/game/game_state.py
@@ -1,13 +1,14 @@
 class GameState:
 
     def __init__(self):
-        self._rounds = 0
-        self._state = {"Circles": 0,
+        self._state = {"Rounds": 0,
+                       "Circles": 0,
                        "Wealth": 0,
+                       "Material": 0,
                        "Mentality": 0}
 
     def __str__(self):
-        str = "Rounds: {}\n".format(self._rounds)
+        str = " -- "
         for k in list(self._state.keys()):
-            str += "{}: {}\n".format(k, self._state[k])
+            str += "{}: {} -- ".format(k, self._state[k])
         return str
